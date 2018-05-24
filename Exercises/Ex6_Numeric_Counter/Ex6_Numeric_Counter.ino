f772ac4600b0ec2b7794fbbb6bc8a78abeb87eb6
Mauricio Tejada <mtejada11@hotmail.com>
2018-05-24 06:12:25 -0500

    Numerical counter example

---
+++ Exercises/Ex6_Numeric_Counter/Ex6_Numeric_Counter.ino
@@ -0,0 +1,38 @@
+
+//========== SEED Electronics/Coding Workshop Exercise 6 ==========
+
+//Purpose: Numeric counter on 5x4 LED matrix
+//Date: May 24, 2018
+
+
+#include "libhardware.h"
+Display display;
+TextDisplay textDisplay(display);
+
+
+int counter = 0;
+
+
+void setup() 
+{
+  //Initialize LED matrix display
+  display.setup(5,4);
+}
+
+
+void loop() 
+{
+  //Create text string from counter value
+  char text[10];
+  itoa(counter, text, 10);
+
+  //Draw text string to display
+  display.clear();
+  textDisplay.displayText(text);
+  delay(500);
+
+  //Increment counter
+  counter++;
+  if (counter > 9) counter = 0;
+}
+
